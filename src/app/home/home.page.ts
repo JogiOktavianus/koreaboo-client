a109ac490ac86f8372d06f8c34917528e0c44bd5
Shiki Rie <shikirie@gmail.com>
2019-04-11 00:22:03 +0700

    Progress
    Image slider

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  username: string;
+  password: string;
+  constructor(public navCtrl: NavController) {
 
+  }
+  login() {
+    console.log("Username: " + this.username);
+    console.log("Password: " + this.username);
+  }
+  register() {
+
+  }
 }
